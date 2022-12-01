700a74fbfaef9ca67b351eab14ee81b8668c3ca8
haycon0 <32018882+haycon0@users.noreply.github.com>
2022-12-01 04:21:49 -0500

    Player

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+onready var SM = $SM
+export var move_speed = 20
+
+var direction = 0
+
+func _physics_process(_delta):
+	if direction < 0 and not $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = true
+	if direction > 0 and $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = false
+		
+
+func is_moving():
+	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		return true
+	return false
+
+func move_vector():
+	return Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up"))
+
+func _unhandled_input(event):
+	if event.is_action_pressed("left"):
+		direction = -1
+	if event.is_action_pressed("right"):
+		direction = 1
+
+func set_animation(anim):
+	if $AnimatedSprite.animation == anim: return
+	if $AnimatedSprite.frames.has_animation(anim): $AnimatedSprite.play(anim)
+	else: $AnimatedSprite.play()
